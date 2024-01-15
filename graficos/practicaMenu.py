fb62ed269e7a5064221e99f09f753014bc167259
Marina Fernández Delgado, PhD <141554145+Hulkilla@users.noreply.github.com>
2024-01-15 22:13:14 +0100

    Add files via upload

--- graficos/practicaMenu.py
+++ graficos/practicaMenu.py
@@ -1,6 +1,32 @@
 from tkinter import *
+from tkinter import messagebox #Hay que importarlo
+
 
 root = Tk()
+
+
+def infoAdicional():
+    messagebox.showinfo("Procesador de Marina", "Programa de hacer cosas version 2023") # Nombre de la ventana emergente y el texto
+                  
+def LicenciaEstado():
+    messagebox.showwarning("Licencia", "Producto bajo licencia GNU")
+
+def QuieresSalir():
+    valor = messagebox.askquestion("Salir", "¿Deseas salir de la aplicacion?")
+    # askquestion devuelve yes o no  
+    
+    if valor == "yes":
+        root.destroy()
+    
+    # valor = messagebox.askokcancel("Salir", "¿Deseas salir de la aplicacion?")
+    # y el valor devuelve TRUE o FALSE
+    
+def cerrarDocumento():
+    valor = messagebox.askretrycancel("Reintentar", "No es posible cerrar. Documento bloqueado")
+
+    if valor == False:
+        root.destroy()
+
 
 barraMenu = Menu(root)
 root.config(menu = barraMenu, width = 300, height=300) #Hay que especificar que se cree el na raiz el menú
@@ -11,8 +37,8 @@ archivoMenu.add_command(label = "Nuevo")
 archivoMenu.add_command(label = "Guardar")
 archivoMenu.add_command(label = "Guardar como")
 archivoMenu.add_separator()
-archivoMenu.add_command(label = "Cerrar")
-archivoMenu.add_command(label = "Salir")
+archivoMenu.add_command(label = "Cerrar", command = cerrarDocumento)
+archivoMenu.add_command(label = "Salir", command = QuieresSalir)
 
 edicionMenu = Menu(barraMenu, tearoff=0)
 edicionMenu.add_command(label = "Copiar")
@@ -23,8 +49,8 @@ edicionMenu.add_command(label = "Pegar")
 herramientasMenu = Menu(barraMenu, tearoff=0)
 
 ayudaMenu = Menu(barraMenu, tearoff=0)
-ayudaMenu.add_command(label = "Licencia")
-ayudaMenu.add_command(label = "Acerca de")
+ayudaMenu.add_command(label = "Licencia", command = LicenciaEstado)
+ayudaMenu.add_command(label = "Acerca de...", command = infoAdicional)
 
 
 #Forma de colocar el menú
